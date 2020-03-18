703011aa879acecd48b1837450c44daded6f890d
xuan <mxuanp@protonmail.com>
2020-03-18 11:47:47 +0800

    修改model中parent的定义，parent应为完整路径

--- main.go
+++ main.go
@@ -62,10 +62,11 @@ func main() {
 
 	router := gin.Default()
 
-	//上传文件
-	router.POST("/api/file", func(c *gin.Context) {
-		form, _ := c.MultipartForm()
+	//上传文件, 或创建文件夹
+	router.POST("/api/file/new", func(c *gin.Context) {
+		flag = c.PostForm("flag")
 		parent := c.PostForm("parent")
+		form, _ := c.MultipartForm()
 		files := form.File["files"]
 
 		if !CheckParent(parent) {
@@ -76,12 +77,34 @@ func main() {
 			return
 		}
 
+		if flag == "directory" {
+			dirName = c.PostForm("dirName")
+			if CheckFile(dirName, parent) {
+				c.JSON(200, gin.H{
+					"status":  "3003",
+					"message": "dir is exist",
+				})
+				return
+			}
+
+			file := model.File{Name: dirName, Size: "4KB", Category: "directory", Parent: parent, Url: ""}
+			db.Create(&file)
+
+			c.JSON(200, gin.H{
+				"status":  "2002",
+				"message": "create successed",
+				"data":    file,
+			})
+
+			return
+		}
+
 		//开启事务, 有上传失败，放弃所有上传文件，返回结果
 		tx := db.Begin()
 
 		for _, file := range files {
-			if CheckFile(file.Filename) {
-				file.Filename =utils.RandString(4) + "_" + file.Filename
+			if CheckFile(file.Filename, parent) {
+				file.Filename = utils.RandString(4) + "_" + file.Filename
 			}
 
 			fields := map[string]string{
@@ -108,15 +131,15 @@ func main() {
 
 			if resp.StatusCode == http.StatusOK {
 				info := utils.ParseSuccessRes(body)
-				tx.Create(&model.File{Name:file.Filename, Size:info.Data.File.Metadata.Size.Readable, Category:"file", Parent:parent, Url:info.Data.File.Url.Full})
+				tx.Create(&model.File{Name: file.Filename, Size: info.Data.File.Metadata.Size.Readable, Category: "file", Parent: parent, Url: info.Data.File.Url.Full})
 
-			}else {
+			} else {
 				tx.Rollback()
 
 				info := utils.ParseErrorRes(body)
 				c.JSON(200, gin.H{
-					"status":info.Err.Code,
-					"message":info.Err.Message,
+					"status":  info.Err.Code,
+					"message": info.Err.Message,
 				})
 				return
 			}
@@ -131,58 +154,28 @@ func main() {
 		})
 	})
 
-	//创建文件夹
-	router.POST("/api/dir", func(c *gin.Context) {
-		dirName := c.PostForm("dirName")
-		parent := c.PostForm("parent")
-
-		if !CheckParent(parent) {
-			c.JSON(200, gin.H{
-				"status":  "3003",
-				"message": "parent dir is not exist",
-			})
-			return
-		}
-
-		if CheckFile(dirName) {
-			c.JSON(200, gin.H{
-				"status":  "3004",
-				"message": "dir was exist",
-			})
-			return
-		}
-
-		file := model.File{Name: dirName, Size: "4KB", Category: "directory", Parent: parent, Url: ""}
-		db.Create(&file)
-
-		c.JSON(200, gin.H{
-			"status":  "2002",
-			"message": "create successed",
-			"data":    file,
-		})
-	})
-
 	//获取文件夹信息, 以及该文件夹下所有文件
-	router.GET("/api/dir/*dirName", func(c *gin.Context) {
-		dir := c.Param("dirName")
+	router.GET("/api/file/info/*fileName", func(c *gin.Context) {
+		file := c.Param("fileName")
 
-		if len(dir) > 1 && dir[0] == '/' { //获取非root / 目录下需要对路径进行处理
-			dir = dir[1:]
-		}
 		//对路径的检查到此，以后逐渐增加其它的路径合法性检查，比如，路径像这样的 adb/adv是不合法的
 
-		files := GetFiels(dir)
+		files, isFile := GetFiles(file)
 
 		c.JSON(200, gin.H{
 			"status":  "2002",
 			"message": "get successed",
-			"data":    files,
+			"data": {
+				"files":  files,
+				"isFile": isFile,
+			},
 		})
 	})
 
 	//更新文件信息
 	router.PUT("/api/file/update", func(c *gin.Context) {
 		fileName := c.PostForm("fileName")
+
 		id, err := strconv.Atoi(c.PostForm("id"))
 		if err != nil {
 			c.JSON(200, gin.H{
@@ -194,8 +187,16 @@ func main() {
 		file := model.File{Model: gorm.Model{ID: uint(id)}}
 		db.First(&file)
 
+		if CheckFile(fileName, file.Parent) {
+			c.JSON(200, gin.H{
+				"status":  "3004",
+				"message": "name was exist",
+			})
+			return
+		}
+
 		if file.Category == "directory" { //要修改的file是文件夹类型，需要同时修改该目录下所有文件的父级目录 parent
-			db.Model(model.File{}).Where("parent = ?", file.Name).Updates(model.File{Parent: fileName})
+			db.Model(model.File{}).Where("parent = ?", file.Parent+"/"+file.Name).Updates(model.File{Parent: file.Parent + "/" + fileName})
 		}
 		file.Name = fileName
 		db.Save(&file)
@@ -208,14 +209,16 @@ func main() {
 	})
 
 	//删除文件或文件夹
-	router.DELETE("/api/file/:fileName", func(c *gin.Context) {
-		fileName := c.Param("fileName")
+	router.DELETE("/api/file/*fileName", func(c *gin.Context) {
+		path := strings.Split(c.Param("fileName"), "/")
+		fileName := path[len(path)-1]
+		parent := strings.Join(path[:len(path)-1], "/")
 
 		var file model.File
-		db.Where("name = ?", fileName).First(&file)
+		db.Where("parent = ? and name = ?", parent, fileName).First(&file)
 
 		if file.Category == "directory" {
-			db.Where("parent = ?", fileName).Delete(model.File{})
+			db.Unscoped().Where("parent = ?", path).Delete(model.File{})
 		}
 
 		db.Unscoped().Delete(&file) //gorm.Model定义了deleted_at, 直接Delete会进行软删除，使用Unscoped设置直接删除记录
@@ -226,30 +229,52 @@ func main() {
 		})
 	})
 
+	//给客户端测试下联通情况
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
 	router.Run()
 }
 
 //GetFiles 查询当前文件夹下所有文件，parent为当前文件夹名
-func GetFiels(parent string) (files []model.File) {
-	db.Where("parent = ?", parent).Find(&files)
+func GetFiles(name string) (files []model.File, isFile bool) {
+	parent := "NULL"
+	filename := name
+	if name != "/" {
+		paths := strings.Split(name, "/")
+		filename = paths[len(paths)-1]
+		parent = strings.Join(paths[:len(paths)-1], "/")
+	}
+
+	var file model.File
+	db.Where("parent = ? and name = ", parent, filename).First(&file)
+
+	if file.Category == "file" {
+		files = append(files, file)
+		isFile = true
+		return
+	}
+
+	db.Where("parent = ?", name).Find(&files)
 	sort.Sort(model.FileSlice(files))
+	isFile = false
 	return
 }
 
-//CheckParent 检查父级文件夹是否存在
 func CheckParent(parent string) bool {
-	return Exist(parent)
+	if parent == "/" {
+		return true
+	}
+	var count int
+	paths := strings.Split(parent, "/")
+	return CheckFile(paths[len(paths)-1], strings.Join(paths[:len(paths)-1], "/"))
 }
 
 //CheckFile 检查文件是否已存在
-func CheckFile(name string) bool {
-	return Exist(name)
-}
-
-//Exist 检查数据库是否已有相同name的file
-func Exist(file string) bool {
+func CheckFile(name, parent string) bool {
 	var count int
-	db.Where("name = ?", file).First(&model.File{}).Count(&count)
+	db.Where("parent = ? and name = ?", parent, name).First(&model.File{}).Count(&count)
 	return count != 0
 }
 
@@ -283,5 +308,3 @@ func UploadFile(url string, f io.Reader, fields map[string]string) (*http.Respon
 
 	return resp, nil
 }
-
-
